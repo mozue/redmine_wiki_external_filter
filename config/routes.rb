a15404184375c8f69e8733d7c8e4b84f45b3502e
Kouhei Sutou <kou@clear-code.com>
2019-01-31 18:14:29 +0900

    Fold long lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
-get 'wiki_external_filter/:filename', :controller => 'wiki_external_filter', :action => 'filter', :macro => 'video', :index => '1', :requirements => { :filename => /video\.flv/ }
-get 'wiki_external_filter/:filename', :controller => 'wiki_external_filter', :action => 'filter', :macro => 'video_url', :index => '1', :requirements => { :filename => /video_url\.flv/ }
-match '/wiki_external_filter', :to => 'wiki_external_filter#filter', :via => [:get, :post]
+get('wiki_external_filter/:filename',
+    :controller => 'wiki_external_filter',
+    :action => 'filter',
+    :macro => 'video',
+    :index => '1',
+    :requirements => { :filename => /video\.flv/ })
+get('wiki_external_filter/:filename',
+    :controller => 'wiki_external_filter',
+    :action => 'filter',
+    :macro => 'video_url',
+    :index => '1',
+    :requirements => { :filename => /video_url\.flv/ })
+match('/wiki_external_filter',
+      :to => 'wiki_external_filter#filter',
+      :via => [:get, :post])
--- routes.rb
+++ routes.rb
@@ -1,1 +1,6 @@
-connect 'wiki_external_filter/:filename', :controller => 'wiki_external_filter', :action => 'filter', :macro => 'flowplayer', :index => '1', :requirements => { :filename => /\S+\.flv/ }
+connect('wiki_external_filter/:filename',
+        :controller => 'wiki_external_filter',
+        :action => 'filter',
+        :macro => 'flowplayer',
+        :index => '1',
+        :requirements => { :filename => /\S+\.flv/ })
